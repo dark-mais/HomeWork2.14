aa5da841d5687c0171f20e41c569076019c6693e
dark-mais <dark1mais@gmail.com>
2024-05-15 21:08:47 +0300

    сделана домашка 2.15

---
+++ src/main/java/org/example/sort/QuickSort.java
@@ -0,0 +1,36 @@
+package org.example.sort;
+
+import org.example.Sortable;
+
+public class QuickSort implements Sortable {
+
+    @Override
+    public void sort(Integer[] array, int size) {
+        quickSort(array, 0, size - 1);
+    }
+
+    private void quickSort(Integer[] array, int left, int right) {
+        if (left < right) {
+            int pivotIndex = partition(array, left, right);
+            quickSort(array, left, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, right);
+        }
+    }
+
+    private int partition(Integer[] array, int left, int right) {
+        int pivot = array[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[right];
+        array[right] = temp;
+        return i + 1;
+    }
+}
